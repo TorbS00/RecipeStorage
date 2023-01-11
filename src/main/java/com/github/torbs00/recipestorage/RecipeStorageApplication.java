1b23e770329cb37c9868101f52526dba07ca2374
BeastyBoo <torbjorn1.skavhaug@gmail.com>
2023-01-11 22:06:22 +0100

    Initial commit

---
+++ src/main/java/com/github/torbs00/recipestorage/RecipeStorageApplication.java
@@ -0,0 +1,13 @@
+package com.github.torbs00.recipestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/torbs00/recipestorage/RecipeStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.torbs00.recipestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
