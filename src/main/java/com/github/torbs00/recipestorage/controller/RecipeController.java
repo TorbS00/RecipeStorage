4408ffc806c0b5f29347a67dbb3bd6e9339022a7
BeastyBoo <torbjorn1.skavhaug@gmail.com>
2023-04-16 18:16:39 +0200

    Added simple support for multiple recipes. Will need to fix poor practices inside RecipeData

--- src/main/java/com/github/torbs00/recipestorage/controller/RecipeController.java
+++ src/main/java/com/github/torbs00/recipestorage/controller/RecipeController.java
@@ -2,24 +2,35 @@ package com.github.torbs00.recipestorage.controller;
 
 import com.github.torbs00.recipestorage.api.RecipeAPI;
 import com.github.torbs00.recipestorage.repository.entity.RecipeData;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class RecipeController implements RecipeAPI {
 
-    private RecipeData recipe;
+    private Map<Integer, RecipeData> recipes = new HashMap<>();
 
     @Override
-    @PostMapping(value = "/recipe", consumes = "application/json")
-    public void addRecipe(@RequestBody RecipeData recipe) {
-        this.recipe = recipe;
+    @PostMapping(value = "/recipe/new", consumes = "application/json")
+    public ResponseEntity<Map<String, Integer>> addRecipe(@RequestBody RecipeData recipe) {
+        recipes.put(recipe.getId(), recipe);
+        return ResponseEntity.ok(Map.of("id", recipe.getId()));
     }
 
     @Override
-    @GetMapping("/recipe")
-    public RecipeData getRecipe() {
-        return recipe;
+    @GetMapping("/recipe/{id}")
+    public ResponseEntity<RecipeData> getRecipe(@PathVariable int id) {
+        RecipeData recipe = recipes.get(id);
+        if(recipe == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(recipe);
     }
 
 }
